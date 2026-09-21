862d4633596b5db589753f14865a221f650c62fd
beacademic <beacademic.ltda@gmail.com>
2026-09-21 13:15:45 -0300

    chore: borrar archivos sin uso y actualizar dependencias (npm audit)

--- app/applet/fetch.mjs
+++
@@ -1,6 +0,0 @@
-import https from 'https';
-https.get('https://syncroedu.beacademics.com/', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log(data));
-});
